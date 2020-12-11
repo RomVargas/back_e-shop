63b29669c08668b8ba0569c82ca656e0c75f3cd7
rom <romvargas81@gmail.com>
2020-12-11 14:33:42 -0600

    se agrega endpoint a graphql

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import compression from 'compression';
 import { createServer } from 'http';
 import environment from './config/environments';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema/index';
+import expressPlayground from 'graphql-playground-middleware-express';
 
 if(process.env.NODE_ENV !== 'production'){
     const env = environment;
@@ -16,9 +19,20 @@ async function init() {
 
     app.use(compression());
 
-    app.get('/', (_, res) => {
-        res.send('on line E-shop MEAN+G');
+    /**se crea servidor apollo con esquema desde schema/intex.ts */
+    const server = new ApolloServer({
+        schema: schema,
+        introspection: true
     });
+
+    /** se aplica el middleware de la aplicacion */
+    server.applyMiddleware({
+        app
+    })
+
+    app.get('/', expressPlayground({
+        endpoint: '/graphql'
+    }));
 
     const httpServer = createServer(app);
     const PORT = process.env.PORT;
