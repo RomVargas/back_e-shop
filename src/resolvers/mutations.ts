728d207de8e27ab37d38be0c700fc5de042319b4
rom <romvargas81@gmail.com>
2021-02-21 13:39:19 -0600

    se agrega validacion de usuario existente

--- src/resolvers/mutations.ts
+++ src/resolvers/mutations.ts
@@ -1,9 +1,21 @@
 import { IResolvers } from "graphql-tools";
 import { COLLECTIONS } from "../config/constants";
+import bcrypt from 'bcrypt';
 
 const resolversMutation: IResolvers  = {
     Mutation: {
         async register(_, { user }, { db }) {
+            // Comporbar que el usuario no exite
+            const userCheck = await db.collection(COLLECTIONS.USERS).findOne({email: user.email});
+
+            if(userCheck !== null){
+                return {
+                    status: false,
+                    message: `El email ${user.email} ya se encuentra registrado`,
+                    user:null
+                }
+            }
+
             // Comporbar el ultimo usuario registardo para asignar ID
             const lastUser = await db.collection(COLLECTIONS.USERS).
                                 find().
@@ -17,13 +29,19 @@ const resolversMutation: IResolvers  = {
             }
             // Asignar la fecha en formato ISO en la propiedead register date
             user.registerDate = new Date().toISOString();
+            // Encriptar password
+            user.password = bcrypt.hashSync(user.password, 12);
 
             //Gusrdar el documento (registro) en la coleccion
             return await db.
                 collection(COLLECTIONS.USERS).
                 insertOne(user).then(
                     async () => {
-                        return user;
+                        return {
+                            status: true,
+                            message:`El email ${user.email} se ha registrado`,
+                            user
+                        };
                     }
                 ).catch((err: Error) => {
                     console.log(err.message);
